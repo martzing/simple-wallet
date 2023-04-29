f1a279c1b900f57ed95fbc9233e3c6b5882749c8
Samart Phrommajaree <martzing25@gmail.com>
2023-04-30 02:21:42 +0700

    src: add cors and helmet

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	helmet "github.com/danielkov/gin-helmet"
+	"github.com/gin-contrib/cors"
 	"github.com/martzing/simple-wallet/configs"
 	"github.com/martzing/simple-wallet/db"
 	"github.com/martzing/simple-wallet/route"
@@ -11,7 +13,10 @@ import (
 func main() {
 	configs.BootConfig()
 	db.Connect(*configs.DbConfig)
+
 	r := gin.Default()
+	r.Use(cors.Default())
+	r.Use(helmet.Default())
 	route.Init(r)
 	r.Run("localhost:9000")
 }
